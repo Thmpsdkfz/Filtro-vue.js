cde835d5d00e9682f27ffedd951dd8d220cf5532
Franciscojho <franciscojho@hotmail.com>
2019-06-06 11:50:15 -0500

    Filtro Básico v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <div class="filtros">
+
+      <label>
+        <input type="radio" name="filterVal" value= "1" v-model="filterVal">
+        Foo Fighters
+      </label>
+      <label>
+        <input type="radio" name="filterVal" value= "2" v-model="filterVal">
+        RHCP
+      </label>
+      <label>
+        <input type="radio" name="filterVal" value= "3" v-model="filterVal">
+        QOTSA
+      </label>
+      <label>
+        <input type="radio" name="filterVal" value= "4" v-model="filterVal">
+        Gorillaz
+      </label>
+
+      <label>
+        <input type="radio" name="filter" value="range1" v-model="rangeFilter">
+          $1 - $10
+      </label>
+      <label>
+        <input type="radio" name="filter" value="range2" v-model="rangeFilter">
+          $11 - $20
+      </label>
+      <label>
+        <input type="radio" name="filter" value="range3" v-model="rangeFilter">
+          $21 - $30
+      </label>
+      <label>
+        <input type="radio" name="filter" value="range4" v-model="rangeFilter">
+          Sobre $30
+      </label>
+
+    </div>
+
+    <div class="display">
+      <div class="cards" v-for="(product, idx) in mainFilter" :key="idx">
+        <h3>{{product.val}}</h3>
+        <small>{{product.by}}</small>
+        <small>{{product.year}}</small>
+        <div class="price"><small>${{product.price}}</small></div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
+import json from './json/bands.json';
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  methods: {
+    getAlbumsByPrice: function(e){
+      if(this.rangeFilter === "range1"){
+        return e.filter(album => album.price>=0 && album.price<=10);
+      }else if(this.rangeFilter === "range2"){
+        return e.filter(album => album.price>10 && album.price<=20);
+      }else if(this.rangeFilter === "range3"){
+        return e.filter(album => album.price>20 && album.price<=30);
+      }else if(this.rangeFilter === "range4"){
+        return e.filter(album => album.price>30);
+      }else{
+        return [];
+      }
+
+    }
+  },
+  computed:{
+    getAlbumsByBand: function(){
+      if(this.filterVal === " "){
+        return this.bands.filter(album => album.parent != 0);
+      }else if(this.filterVal != 0){
+        return this.bands.filter(album => album.parent === parseInt(this.filterVal));
+      }
+    },
+    mainFilter: function(){ 
+      if(this.getAlbumsByBand.length > 0){
+        if(this.rangeFilter==="range1"||this.rangeFilter ==="range2"||this.rangeFilter ==="range3"||this.rangeFilter ==="range4"){
+          return this.getAlbumsByPrice(this.getAlbumsByBand);
+        }
+        return this.getAlbumsByBand;
+      }
+    }
+  },
+  data: function(){
+    return {
+      bands: json,
+      filterVal: " ",
+      rangeFilter: " "
+    }
   }
 };
 </script>
@@ -24,5 +108,52 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: row;
 }
+
+.filtros{
+  width: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: start;
+  margin: 5px;
+
+  border: solid 2px black;
+}
+
+.display{
+  width: 90%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px;
+}
+
+.cards{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 150px;
+  margin: 10px;
+  border: solid 1px black;
+  h3{
+    font-size: .8rem;
+    line-height: .8rem;
+    text-transform: uppercase;
+    margin: 4px 0 4px 0;
+  }
+  .price{
+    display: flex;
+    justify-content: flex-end;
+    small{
+      margin: 0 .4rem .2rem 0;
+    }
+  }
+
+}
+
+
 </style>
